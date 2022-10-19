e34ae4af332b58a399b87d06bdc6aeaf6af76124
Hoàng Gia Trương Cao <truongcaohoanggia@gmail.com>
2022-10-19 11:01:15 +0700

    config run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python main.py level_1 1 gbfs
 python main.py level_1 1 dfs
+python main.py level_1 1 gbfs
 python main.py level_1 1 astar
-python main.py level_1 2 gbfs
 python main.py level_1 2 dfs
+python main.py level_1 2 gbfs
 python main.py level_1 2 astar
