37d5cdab3152aa81856a1a3bc2b0c4551a25c6b6
Hoàng Gia Trương Cao <truongcaohoanggia@gmail.com>
2022-10-21 21:07:18 +0700

    draw all mazes and run all algorithms

--- run.sh
+++ run.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
+python main.py level_1 1 bfs
 python main.py level_1 1 dfs
+python main.py level_1 1 ucs
 python main.py level_1 1 gbfs
 python main.py level_1 1 astar
+#
+python main.py level_1 2 bfs
 python main.py level_1 2 dfs
+python main.py level_1 2 ucs
 python main.py level_1 2 gbfs
 python main.py level_1 2 astar
+#
+python main.py level_1 3 bfs
+python main.py level_1 3 dfs
+python main.py level_1 3 ucs
+python main.py level_1 3 gbfs
+python main.py level_1 3 astar
+#
+python main.py level_1 4 bfs
+python main.py level_1 4 dfs
+python main.py level_1 4 ucs
+python main.py level_1 4 gbfs
+python main.py level_1 4 astar
+#
+python main.py level_1 5 bfs
+python main.py level_1 5 dfs
+python main.py level_1 5 ucs
+python main.py level_1 5 gbfs
+python main.py level_1 5 astar
